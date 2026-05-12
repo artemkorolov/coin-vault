1b7290819628a0dfcb318df3ec5d3f9712df269e
Artem Korolov <adkorolov@gmail.com>
2026-05-12 09:03:02 +0300

    feat: add price change indicators with dynamic colors

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
-function Coin({ image, name, price }) {
+function Coin({ image, name, price, priceChange }) {
+	const priceColor = priceChange > 0 ? "green" : "red";
+
 	return (
 		<div className="coin-item">
 			<img src={image} alt={name} />
 			<span className="coin-name">{name}</span>
 			<span className="coin-price">${price?.toLocaleString()}</span>
+			<span className={`coin-percent ${priceColor}`}>
+				{priceChange.toFixed(2)}%
+			</span>
 		</div>
 	)
 }
